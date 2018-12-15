ed9c8adbd9a3a3a380ea5fbb4538698430d2fc94
Alexander Molnar <mathic@gmail.com>
2018-12-14 23:14:17 -0500

    Add beaver

--- src/components/Project3.js
+++ src/components/Project3.js
@@ -10,7 +10,39 @@ export default (props) => (
         return false;
       }}
     >
-      Proj{props.number}!
+      <div className='beaver-container'>
+        <div className='beaver'>
+  
+          <div className="beaver--body">
+            <div className='beaver--body-copy'></div>
+
+            <div className='beaver--left-ear'></div>
+            <div className='beaver--right-ear'></div>
+            <div className='beaver--left-eye'>
+              <div className='beaver--left-pupil'></div>
+            </div>
+            <div className='beaver--right-eye'>
+              <div className='beaver--right-pupil'></div>
+            </div>
+            <div className='beaver--nose'></div>
+            <div className='beaver--left-mouth'></div>
+            <div className='beaver--right-mouth'></div>
+            <div className='beaver--left-tooth'></div>
+            <div className='beaver--right-tooth'></div>
+            
+            <div className='beaver--left-arm'></div>
+            <div className='beaver--right-arm'></div>
+            
+            <div className='beaver--bottom-body'>
+              <div className='beaver--tail'></div>
+              <div className='beaver--left-mark-one'></div>
+              <div className='beaver--left-mark-two'></div>
+              <div className='beaver--right-mark-one'></div>
+              <div className='beaver--right-mark-two'></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
